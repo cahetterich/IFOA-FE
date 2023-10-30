1cd83058fee5970504d8618fda6dc4981ca7a8b2
cahetterich <hetterichcarla@gmail.com>
2023-10-30 20:43:17 +0100

    Codici lezione settimana 2 giorno 1

--- sett2/G1/Pratica/D1.js
+++ sett2/G1/Pratica/D1.js
@@ -1,6 +1,5 @@
-document.getElementById('titolo').innerHTML = '<em>Esercizi JavaScript</em>';
-/*
-REGOLE
+//document.getElementById('titolo').innerHTML = '<em>Esercizi JavaScript</em>';
+/*REGOLE
 - Tutte le risposte devono essere scritte in JavaScript
 - Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
 - Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
@@ -11,54 +10,54 @@ REGOLE
 /* ESERCIZIO 1
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
-document.getElementById("esercizio1").innerHTML = '<em>1 - I principali datatype in JavaScript:</em>';
+//document.getElementById("esercizio1").innerHTML = '<em>1 - I principali datatype in JavaScript:</em>';
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 // string 
-document.getElementById("valoreStringa").innerHTML = '<em><b>String:</b> utilizzato per rappresentare e manipolare una sequenza di caratteri.</em>';
+//document.getElementById("valoreStringa").innerHTML = '<em><b>String:</b> utilizzato per rappresentare e manipolare una sequenza di caratteri.</em>';
 //number 
-document.getElementById("valoreNumeri").innerHTML = '<em><b>Number:</b> utilizzato per rappresentare numeri in virgola mobile, come ad esempio 37 o -9,25.</em>';
+//document.getElementById("valoreNumeri").innerHTML = '<em><b>Number:</b> utilizzato per rappresentare numeri in virgola mobile, come ad esempio 37 o -9,25.</em>';
 //boolean 
-document.getElementById("valoreBooleani").innerHTML = '<em><b>Boolean:</b> rappresenta un valore di verità: vero o falso.</em>';
+//document.getElementById("valoreBooleani").innerHTML = '<em><b>Boolean:</b> rappresenta un valore di verità: vero o falso.</em>';
 //null 
-document.getElementById("valoreNull").innerHTML = '<em><b>Null:</b> rappresenta la mancanza intenzionale di qualsiasi valore di un oggetto.</em>';
+//document.getElementById("valoreNull").innerHTML = '<em><b>Null:</b> rappresenta la mancanza intenzionale di qualsiasi valore di un oggetto.</em>';
 //undefined
-document.getElementById("valoreUndefined").innerHTML = '<em><b>Undefined:</b> rappresenta il valore primitivo undefined.</em>';
+//document.getElementById("valoreUndefined").innerHTML = '<em><b>Undefined:</b> rappresenta il valore primitivo undefined.</em>';
 
 /* ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.*/
-document.getElementById("esercizio2").innerHTML = '<em> 2 - Crea una variable chiamata "name": </em>';
+//document.getElementById("esercizio2").innerHTML = '<em> 2 - Crea una variable chiamata "name": </em>';
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let myName = 'Carla';
-console.log(myName);
+//let myName = 'Carla';
+//console.log(myName);
 
-document.getElementById("myName").innerHTML += myName;
+//document.getElementById("myName").innerHTML = myName;
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.*/
- document.getElementById("esercizio3").innerHTML = '<em> 3 - Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20: </em>';
+ //document.getElementById("esercizio3").innerHTML = '<em> 3 - Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20: </em>';
 
  /* SCRIVI QUI LA TUA RISPOSTA */
- let numero1 = 12;
- let numero2 = 20;
- let somma = numero1 += numero2
+ //let numero1 = 12;
+ //let numero2 = 20;
+ //let somma = numero1 += numero2
 
-document.getElementById("somma").innerHTML = somma;
+//document.getElementById("somma").innerHTML = somma;
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.*/
- document.getElementById("esercizio4").innerHTML = '<em> 4 - Crea una variable di nome "x" e assegna ad essa il numero 12: </em>';
+ //document.getElementById("esercizio4").innerHTML = '<em> 4 - Crea una variable di nome "x" e assegna ad essa il numero 12: </em>';
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let pi = myName += 12;
+//let pi = myName += 12;
 
-document.getElementById("p").innerHTML = pi;
+//document.getElementById("p").innerHTML = pi;
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.*/
-  document.getElementById("esercizio5").innerHTML = '<em> 5 - Nuovo valore alla variabile: </em>';
+  //document.getElementById("esercizio5").innerHTML = '<em> 5 - Nuovo valore alla variabile: </em>';
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
