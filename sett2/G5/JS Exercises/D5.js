7bdffb5aeb58f449d92df73e948e686dd9500dda
cahetterich <hetterichcarla@gmail.com>
2023-11-06 16:43:10 +0100

    Codici lezione settimana 2 giorno 5

--- sett2/G5/JS Exercises/D5.js
+++ sett2/G5/JS Exercises/D5.js
@@ -111,12 +111,13 @@ for (let i = 0; i < cars.length; i++) {
 
 console.log('Esercizio 7');
 console.log(justTrims);
+
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
 
-console.log('Esercizio 7');
+console.log('Esercizio 8');
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
