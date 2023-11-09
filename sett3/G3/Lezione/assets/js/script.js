83d89f8e51da60fb6980db34a49d6155ee1d2e32
cahetterich <hetterichcarla@gmail.com>
2023-11-09 15:39:23 +0100

    Codici lezione settimana 3 giorno 3

--- sett3/G3/Lezione/assets/js/script.js
+++ sett3/G3/Lezione/assets/js/script.js
@@ -1,15 +1,15 @@
 const btn = document.getElementById('invia');
-
-btn.addEventListener('click', function (e) {
-	e.preventDefault();
-	let form = document.querySelector('form');
+// * ADDEVENTLISTERNER - ativa uma volta, e fine até quando deve ocorrer * CLICK = ação
+btn.addEventListener('click', function (e) {  // METODO GET = MANDA DADOS E BUSCA FILE 
+	//e.preventDefault();
+	let form = document.querySelector('form');  // no momento do click dizemos todos os dados incluidos e sua validade segundo o que escrevemos no HTML
 	let nome = document.getElementById('nome');
 	let cognome = document.getElementById('cognome');
 	let email = document.getElementById('email');
 	let eta = document.getElementById('eta');
 
 	if (!cognome.checkValidity()) {
-		cognome.style.backgroundColor = 'red';
+		cognome.style.backgroundColor = 'red';  // ESCREGVE EM ROSSO O QUE NÃO ESTIVER INSERIDO NO CAMPO
 		document.getElementById('nomeForm').innerText =
 			cognome.validationMessage;
 		return;
@@ -23,8 +23,8 @@ btn.addEventListener('click', function (e) {
 		return;
 	}
 
-	cognome.style.backgroundColor = 'transparent';
-	email.style.backgroundColor = 'transparent';
+	cognome.style.backgroundColor = 'transparent';  // SE TUDO ESTIVER CERTO, VOLTA A SER TRANSPARENTE
+	email.style.backgroundColor = 'transparent';  // MAS DEVEMOS COLCOAR TUDO TRANSPARENTE
 	eta.style.backgroundColor = 'transparent';
 
 	nome = nome.value;
@@ -32,7 +32,7 @@ btn.addEventListener('click', function (e) {
 	email = email.value;
 	eta = eta.value;
 
-	confirm('Sei sicuro dei dati inseriti?');
+	confirm('Sei sicuro dei dati inseriti?');   // CONFIRMAR DADOS ? 
 
 	if (confirm) {
 		document.getElementById('nomeForm').innerText = nome;
@@ -40,68 +40,68 @@ btn.addEventListener('click', function (e) {
 		document.getElementById('emailForm').innerText = email;
 		document.getElementById('etaForm').innerText = eta;
 	} else {
-		form.reset();
+		form.reset();  // COM TUDO CERTO, CANCELA O FORM
 	}
 });
 
 // setTimeout
 
-let btnRigenera = document.getElementById('ricrea');
-function generaCodice() {
+let btnRigenera = document.getElementById('ricrea');  //BUSCA
+function generaCodice() {   // GERA CÓDIGO
     btnRigenera.setAttribute('disabled', 'true');
     document.getElementById('rigenera').innerText = ''
     document.getElementById('codice').style.textDecoration = 'none';
 
     let codice = Math.floor(Math.random() * 100001);
-    if(codice < 100000) {
+    if(codice < 100000) {   // SE O CÓDIGO FOR MENOR QUE 100000, ADICIONA RANDOM 100000 (CÓDIGO DE 6 DIGITOS)
         codice = codice + 100000;
     }
-    document.getElementById('codice').innerText = codice;
-    scadenza();
+    document.getElementById('codice').innerText = codice;  // ESCREVEMOS O CÓDIGO
+    scadenza(); // E DETERMINAMOS A SUA ESCADENZA
 }
 
-const scadenza = (() => {
+const scadenza = (() => {    // DETERMINAMOS O NOME DO TIMEOUT
     setTimeout(() => {
-        rigenera();
-    }, 5000);
+        rigenera();  // SETTIMEAOUT DEVE FAZ - O QUE DEVE ACONTECER - function rigenera ()
+    }, 5000);  // POR QUANTO MILESEGUNDOS DEVE FAZER? 
 });
 
-// function scadenza() {
+// function scadenza() {    
 //     setTimeout(() => {
 //         rigenera();
 //     }, 5000);
 // }
 
-function rigenera() {
+function rigenera() {  // APÓS OS MILESEGUNDOS, O CÓDIGO CANCELA E DEVEMOS GERAR UM NOVO código
     document.getElementById('codice').style.textDecoration = 'line-through';
-    document.getElementById('rigenera').innerText = 'Codice scaduto, rigenerare!'
+    document.getElementById('rigenera').innerText = 'Codice scaduto, rigenerare!' // AVISO AO UTENTI CODIGO SCADUTO
     btnRigenera.removeAttribute('disabled');
 }
 
-generaCodice();
+generaCodice(); // PUXA NOVAMENTE O INICIO DA FUNÇÃO, PARA PODER GERAR UM NOVO CÓDIGO! 
 
-// setTimeout
+// setInterval 
 
-let barra = document.getElementById('barra');
+let barra = document.getElementById('barra');  // CHAMAMOS A BARRA
 
-const avviaTimer = (() => {
-    let timer = 0;
+const avviaTimer = (() => {  // DETERMINAMOS UMA CONST
+    let timer = 0;  // ELA VAI PARTIR DA ZERO
     barra.style.width = '0px';
-    document.getElementById('stop').innerText = '';
+    document.getElementById('stop').innerText = ''; // ESSA BARRA ESTÁ COM ERRO ** POIS NÃO PARA QUANDO SOLICITADO /STOP/
     document.getElementById('percentuale').innerText = '';
     document.getElementById('stop').innerText = '';
 
-    setInterval(() => {
-        if (timer < 501) {
-            barra.style.width = `${timer}px`;
-            let percentuale = timer / 5;
-            document.getElementById('percentuale').innerText = `${percentuale}`
-            timer++;
-        } else {
+    setInterval(() => {  // AQUI PUXARÁ O QUE VAI ACONTECER
+        if (timer < 501) { // POR QUANTO TEMPO < 501 
+            barra.style.width = `${timer}px`;  // AUMENTA A LARGURA DA BARRA
+            let percentuale = timer / 5;  // AUMENTA INCREMENTA 5 PX  POR MILISEGUNDO 
+            document.getElementById('percentuale').innerText = `${percentuale}` // MOSTRA O PERCENTUAL DE CARREGAMENTO DA BARRA
+            timer++;  //  DEPOIS DE 10 MILISEGUNDOS  TIMER++
+        } else { // SERVE PARA BLOCAR, SE NÃO TIVER O IF E ELSE FICARIA CARREGANDO A BARRA INFINITAMENTE
             document.getElementById('stop').innerText = 'Progress Bar interrotta';
-            clearInterval(avviaTimer);
+            clearInterval(avviaTimer); // APÓS TERMIANR O CARREGAMENTO, FIRMA QUI! 
         }
-    }, 10);
+    }, 10); // EM QUANTO TEMPO - 10 MILISEGUNDOS 
 });
 
 // avviaTimer();
